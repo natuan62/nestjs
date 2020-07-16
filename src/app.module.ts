f2b150f100dbab09b535a410be94f0e6e3d93970
anhtuan <anhtuan6294@gmail.com>
2020-07-16 16:55:10 +0700

    Add auth + user model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
+import { UsersModule } from './users/users.module';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomerModule } from './customer/customer.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://admin:123@cluster0-wyxg3.mongodb.net/todo?retryWrites=true&w=majority', { useNewUrlParser: true }),
-    CustomerModule
+    CustomerModule,
+    AuthModule,
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
